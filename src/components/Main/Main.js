5408f99613cee00baea0e74d3ae0f5399a7087e7
tanverrana <tanversobur@gmail.com>
2022-04-06 14:34:39 +0600

    search result

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import useVolunteers from '../../Hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import "./Main.css";
 
 const Main = () => {
     const [volunteers, setVolunteers] = useVolunteers();
+    const [searchResult, setSearchResult] = useState([])
+    const handleSearchChange = event => {
+        const searchText = event.target.value;
+        const match = volunteers.filter(v => v.title.includes(searchText));
+        setSearchResult(match);
+    }
 
 
     return (
         <div>
             <h2>Volunteers ={volunteers.length}</h2>
+            <div className="input-field">
+                <input onChange={handleSearchChange} type="text" placeholder="search here" />
+            </div>
             <div className="activity-container">
                 {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
                         key={activity._id}
                         activity={activity}
                     ></Activity>)
