60574fe9162db79c3d4ee2de40e4ce04bb9fb0bf
tanverrana <tanversobur@gmail.com>
2022-04-06 12:04:04 +0600

    state change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Home = () => {
+    const [count, setCount] = useState(0);
+    const increaseCount = () => {
+        setCount(count + 1);
+    }
+    const number = 56;
     return (
         <div>
-            <button>Click Me</button>
+            <button onClick={increaseCount}>Click Me</button><br />
+            <p>Count={count}</p>
+            <h4>My Number={number}</h4>
+            <h2>Double Count={count * 2}</h2>
         </div>
     );
 };
